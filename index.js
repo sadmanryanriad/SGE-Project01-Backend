c11f9ccd2ff9e561234f604c3d6537e392fdfbc1
sadmanryanriad <sadmanryanriad@gmail.com>
2024-07-05 11:48:37 +0600

    database connected with mongoose

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
-
-const port = process.env.PORT || 5000;
+require("dotenv").config();
+const port = process.env.PORT || process.env.LP;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ni8nft9.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
 
 //middlewares
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-  res.status(200).json("HOME PAGE");
-});
+//database connection
+mongoose
+  .connect(uri)
+  .then(() => {
+    //routes
 
-app.listen(port, () => {
-  `listening on port: ${port}`;
-});
+    app.get("/", async (req, res) => {
+      res.status(200).json("HOME PAGE");
+    });
+
+    app.listen(port, () => {
+      console.log(`Connected to database and listening on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
