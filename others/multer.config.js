7ff2774b90ddbed851962567bca47052011c2ad3
sadmanryanriad <sadmanryanriad@gmail.com>
2024-07-12 22:37:18 +0600

    file directly upload to cloudinary, new package: streamifier to manage buffer

--- others/multer.config.js
+++ others/multer.config.js
@@ -1,16 +1,8 @@
 // multerConfig.js
 const multer = require("multer");
-const path = require("path");
 
-// Configure storage for multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Save files in the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Append timestamp to the original file name
-  },
-});
+// Configure storage for multer to use memory storage
+const storage = multer.memoryStorage();
 
 // Initialize multer with the storage configuration
 const upload = multer({ storage: storage });
--- uploads/upload.js
+++
@@ -1,4 +0,0 @@
-const str =
-  "this is important. if this is not uploaded into the vercel it will not work";
-
-module.exports = str;
