828fa705f771d9f43bf29e81efab0c9ce7d35d33
sadmanryanriad <sadmanryanriad@gmail.com>
2024-07-17 13:11:30 +0600

    paymentScheduler updated with proper logic

--- others/paymentScheduler.js
+++ others/paymentScheduler.js
@@ -6,28 +6,55 @@ const jobs = {};
 
 const scheduleJob = (studentId, delay) => {
   const job = cron.schedule(`*/1 * * * * *`, async () => {
-    const student = await Student.findById(studentId);
-    if (student && student.status.status === "enrollment") {
-      const secondsSinceEnrollment =
-        (new Date() - new Date(student.enrollmentStartDate)) / 1000; //milliseconds to seconds
-      // console.log(secondsSinceEnrollment);
-      if (secondsSinceEnrollment >= 15) {
-        // 15 seconds for testing
-        const currentStudent = await Student.findById(studentId);
-        if (currentStudent.status.status === "enrollment") {
-          await Student.findByIdAndUpdate(studentId, {
-            paymentStatus: "to be paid",
-          });
+    try {
+      const student = await Student.findById(studentId);
+      if (student && student.status.status === "enrollment") {
+        const secondsSinceEnrollment =
+          (new Date() - new Date(student.enrollmentStartDate)) / 1000; // milliseconds to seconds
+
+        if (secondsSinceEnrollment >= 15) {
+          const currentStudent = await Student.findById(studentId);
+          if (currentStudent.status.status === "enrollment") {
+            //find member and calculate money
+            let money = 300;
+            const member = await Member.findOne({ email: student.createdBy });
+            if (!member) console.log("member not found");
+            //if enrolled student count is greater than 4
+            if (member.enrolledStudents.length >= 4) {
+              money = 400;
+            }
+            //if enrolled student count is greater than 15
+            if (member.enrolledStudents.length >= 15) {
+              money = 500;
+            }
+
+            // Change student payment status with amount
+            await Student.findByIdAndUpdate(studentId, {
+              paymentStatus: money,
+            });
+
+            // Add enrollment data to the member
+            await Member.findOneAndUpdate(
+              { email: student.createdBy },
+              { $push: { enrolledStudents: student._id } }
+            );
+
+            console.log(
+              `Enrollment added to member:${member.firstName} ${member.lastName} <-> payment status updated for student: ${student.firstName} ${student.lastName} <-> amount: ${money}`
+            );
+
+            // Stop the job and delete it
+            job.stop();
+            delete jobs[studentId];
+          }
         }
-        job.stop();
-        delete jobs[studentId];
-        //add enrollment data to the member
-        const rr = await Member.findOneAndUpdate(
-          { email: student.createdBy },
-          { $push: { enrolledStudents: student._id } }
-        );
-        console.log("enrollment added to member");
       }
+    } catch (error) {
+      console.error("Error processing scheduled job:", error);
+
+      // Stop the job and delete it on error
+      job.stop();
+      delete jobs[studentId];
     }
   });
 
